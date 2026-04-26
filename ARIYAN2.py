80da17ff3dc0dc368c54c4806e7a8070c1d677de
tureydrip <juliangoza234rg@gmail.com>
2026-04-26 12:47:08 -0500

    Update ARIYAN2.py

--- ARIYAN2.py
+++ ARIYAN2.py
@@ -1,12 +1,21 @@
 import requests
+import urllib3
 from google_play_scraper import app
 
+# Esto desactiva las advertencias de seguridad en la consola de Railway
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 def AuToUpDaTE():
+    # Obtiene la versión actual del juego desde la Play Store
     result = app('com.dts.freefireth', lang="fr", country='fr')
     version = result['version']
+    
+    # Se agrega verify=False para saltar la verificación del certificado SSL dañado
     r = requests.get(
-        f'https://bdversion.ggbluefox.com/live/ver.php?version={version}&lang=ar&device=android&channel=android&appstore=googleplay&region=ME&whitelist_version=1.3.0&whitelist_sp_version=1.0.0&device_name=google%20G011A&device_CPU=ARMv7%20VFPv3%20NEON%20VMH&device_GPU=Adreno%20(TM)%20640&device_mem=1993'
+        f'https://bdversion.ggbluefox.com/live/ver.php?version={version}&lang=ar&device=android&channel=android&appstore=googleplay&region=ME&whitelist_version=1.3.0&whitelist_sp_version=1.0.0&device_name=google%20G011A&device_CPU=ARMv7%20VFPv3%20NEON%20VMH&device_GPU=Adreno%20(TM)%20640&device_mem=1993',
+        verify=False
     ).json()
+    
     url = r['server_url']
     ob = r['latest_release_version']
     return url, ob, version
